a859d2c5bb671bd9715a874db6d8149a6e0fc8e1
Lina Kuhn <lina.kuhn@tngtech.com>
2023-12-02 17:29:20 +0100

    day 1

---
+++ day_01/day_01_python.py
@@ -0,0 +1,36 @@
+import re
+
+
+def word_or_number_to_int(number_word):
+    word_to_int = {
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9,
+        'zero': 0,
+    }
+    return word_to_int.get(number_word) if number_word in word_to_int.keys() else int(number_word)
+
+
+def sum_first_and_last_number(numbers):
+    return sum(
+        [word_or_number_to_int(number[0]) * 10 + word_or_number_to_int(number[-1]) for number in numbers])
+
+
+def read_input_with_pattern(filename, pattern):
+    with open(filename, "r") as file:
+        return [re.findall(pattern, line.strip()) for line in file.readlines()]
+
+
+## part 1
+numbers_in_list = read_input_with_pattern("input.txt", r"\d")
+print(f"Part 1: {sum_first_and_last_number(numbers_in_list)}")
+
+## part 2
+numbers_in_list = read_input_with_pattern("input.txt", r"(one|two|three|four|five|six|seven|eight|nine|zero|\d)")
+print(f"Part 2: {sum_first_and_last_number(numbers_in_list)}")
---
+++ day_01/day_01_typescript.ts
@@ -0,0 +1,50 @@
+import * as fs from "fs";
+
+export class SolutionDayOne {
+
+    readInput() {
+        return fs.readFileSync("input.txt", "utf-8").split(/\n/);
+    }
+
+    findAllMatches(pattern: RegExp, line: string) {
+        let match = pattern.exec(line);
+        let matches: string[] = [];
+        while(match) {
+            matches.push(match[0]);
+            match = pattern.exec(line);
+        }
+        return matches
+    }
+
+    getCalibrationValue(numbers: string[]): number {
+        return this.stringToNumber(numbers.at(0))*10 + this.stringToNumber(numbers.at(-1));
+    }
+
+    stringToNumber(stringNumber: string): number {
+        const numberLiterals = new Map([
+            ["one",1],["two",2],["three",3],["four",4],["five",5],["six",6],["seven",7],["eight",8],["nine",9],["zero",0]
+        ])
+        return numberLiterals.has(stringNumber)? (numberLiterals.get(stringNumber)): (+stringNumber)
+    }
+
+    computeSolution(pattern: RegExp) {
+        const numbersInLines: string[][] = this.readInput().map((value: string, index: number) => {return this.findAllMatches(pattern, value)});
+        const calibrationValues: number[] = numbersInLines.map((value: string[], index: number) => {return this.getCalibrationValue(value)});
+        return calibrationValues.reduce((sum: number, p: number) => sum + p );
+    }
+
+    partOne() {
+        const patternPartOne: RegExp = new RegExp(/\d/, "g",);
+        return this.computeSolution(patternPartOne);
+    }
+
+    partTwo() {
+        const patternPartTwo: RegExp = new RegExp(/(one|two|three|four|five|six|seven|eight|nine|zero|\d)/, "g");
+        return this.computeSolution(patternPartTwo);
+    }
+}
+
+const solutionDayOne = new SolutionDayOne();
+console.log(solutionDayOne.partOne());
+console.log(solutionDayOne.partTwo());
+
